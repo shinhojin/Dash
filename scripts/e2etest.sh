41b56c6cb67aec0a14d9c13681b9b7f48acf37d1
namhc616 <nhc616@gmail.com>
2019-10-27 21:48:53 -0700

    update scripts

--- scripts/e2etest.sh
+++ scripts/e2etest.sh
@@ -20,22 +20,16 @@ verifyResult () {
 	fi
 }
 
-# PEER0 for consumer, PEER1 for retailer
-# PEER2 for logistics, PEER3 for wholesaler
-# PEER4 for manufacture processor, PEER5 for product producer
 setGlobals () {
 	ORG=$2
 	if [ $ORG -eq 1 ] ; then
 		CORE_PEER_LOCALMSPID="Org1MSP"
 		CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.supply.com/peers/peer0.org1.supply.com/tls/ca.crt
 		CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.supply.com/users/Admin@org1.supply.com/msp
-		#consumer and retailer on shopping site 
 		if [ $1 -eq 0 ]; then
-			#consumer
 			CORE_PEER_ADDRESS=peer0.org1.supply.com:7051
 			PEER=PEER0
 		else
-			#retailer
 			CORE_PEER_ADDRESS=peer1.org1.supply.com:7051
 			PEER=PEER1
 		fi
@@ -43,13 +37,10 @@ setGlobals () {
 		CORE_PEER_LOCALMSPID="Org2MSP"
 		CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.supply.com/peers/peer0.org2.supply.com/tls/ca.crt
 		CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.supply.com/users/Admin@org2.supply.com/msp
-		#logistics and wholesaler
 		if [ $1 -eq 0 ]; then
-			#logistics
 			CORE_PEER_ADDRESS=peer0.org2.supply.com:7051
 			PEER=PEER2
 		else
-			#wholesaler
 			CORE_PEER_ADDRESS=peer1.org2.supply.com:7051
 			PEER=PEER3
 		fi
@@ -57,13 +48,10 @@ setGlobals () {
 		CORE_PEER_LOCALMSPID="Org3MSP"
 		CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.supply.com/peers/peer0.org3.supply.com/tls/ca.crt
 		CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.supply.com/users/Admin@org3.supply.com/msp
-		#manufacture processor and raw food producer
 		if [ $1 -eq 0 ]; then
-			#manufacture processor
 			CORE_PEER_ADDRESS=peer0.org3.supply.com:7051
 			PEER=PEER4
 		else
-			#raw food producer
 			CORE_PEER_ADDRESS=peer1.org3.supply.com:7051
 			PEER=PEER5
 		fi		
